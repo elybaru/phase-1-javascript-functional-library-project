251f8596fdd16cb94e9441de23e5a6188f468d0e
Ely Karr <elykarr@gmail.com>
2021-10-29 11:24:36 -0700

    Done.

--- index.js
+++ index.js
@@ -28,41 +28,52 @@ function myMap(collection, callback) {
 
 }
 
+//Instructor-provided solution
+// const myReduce = function (collection, callback, acc) {
+//     let newCollection = standardizeInput(collection);
+//     if (!acc) {
+//         acc = newCollection[0];
+//         newCollection = newCollection.slice(1);
+//     }
+
+//     const len = newCollection.length;
+
+//     for (let i = 0; i < len; i++) {
+//         acc = callback(acc, newCollection[i], newCollection);
+//     }
+//     return acc;
+// }
+
+// My solution
+
 function myReduce(collection, callback, acc) {
     let newCollection = standardizeInput(collection)
-
     // acc ? results = acc : results = newCollection[0];
-
     if (!acc) {
         acc = newCollection[0]
-        newCollection = newCollection.shift()
+        newCollection = newCollection.slice(1)
     }
-    console.log(acc)
-
-
+    // console.log(acc)
     for (let i = 0; i < newCollection.length; i++) {
 
         acc = callback(acc, newCollection[i], newCollection)
-        console.log("ACC in the FOR loop " + acc)
+        // console.log("ACC in the FOR loop " + acc)
     }
-
     return acc
-
-
 }
 
-const callback = (acc, val, collection) => (acc + (val * 3))
-myReduce([1, 2, 3, 4], callback, 10)
+// const callback = (acc, val, collection) => (acc + (val * 3))
+// myReduce([1, 2, 3, 4], callback, 10)
 
-function myFind(collection, predicate) {
-    let newCollection = standardizeInput(collection)
+// function myFind(collection, predicate) {
+//     let newCollection = standardizeInput(collection)
 
 
-    for (const element of newCollection) {
-        if (predicate(element)) {
-            return element
-        }
+for (const element of newCollection) {
+    if (predicate(element)) {
+        return element
     }
+}
 }
 
 function myFilter(collection, predicate) {
@@ -95,18 +106,40 @@ function mySize(collection) {
 
 
 
-function myFirst() {
+function myFirst(collection, n) {
+    let newCollection = standardizeInput(collection)
+
+    if (!n) {
+        return newCollection[0]
+    } else {
+        for (let i = 0; i <= n; i++) {
+            return newCollection[i]
+        }
+    }
 
 }
 
-function myLast() {
+function myLast(collection, n) {
+    let newCollection = standardizeInput(collection)
+
+    if (!!n) {
+        return newCollection[0]
+    } else {
+        return newCollection[0]
+    }
 
 }
 
 
 // Don't use the top function, not applicable here, and we know they will be referring to objects
-function myKeys() {
+function myKeys(obj) {
 
+    for (let i = 0; i <= n; i++) {
+        return obj[0]
+        if (i % 2 === 0) {
+            return obj[i]
+        }
+    }
 }
 
 function myValues() {
