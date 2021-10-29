6ce0726a7c5b3b19e6885c793adbe39e1c0688eb
Ely Karr <elykarr@gmail.com>
2021-10-28 17:57:58 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,113 @@
+const standardizeInput = function (collection) {
+    return (collection instanceof Array) ? collection.slice() : Object.values(collection)
+}
+
+// instanceof looks at the collection to see if it's an Array, then it slice returns a shallow copy of the array, if falsey, then returns the values of the object pairs
+
+
+function myEach(collection, callback) {
+    let newCollection = standardizeInput(collection)
+    //checks if it's an array or object then returns it- if it's an object, then it turns it into an array
+
+
+    for (let i = 0; i < newCollection.length; i++) {
+        callback(newCollection[i])
+    }
+    return collection
+}
+
+
+function myMap(collection, callback) {
+    let newCollection = standardizeInput(collection)
+    let newMappedArray = []
+
+    for (let i = 0; i < newCollection.length; i++) {
+        newMappedArray.push(callback(newCollection[i]))
+    }
+    return newMappedArray
+
+}
+
+function myReduce(collection, callback, acc) {
+    let newCollection = standardizeInput(collection)
+
+    let results;
+
+    acc ? results = acc : results = 0;
+    console.log("RESULTS BEFORE FOR LOOP " + results)
+    let x = 0
+
+    for (let i = 0; i < newCollection.length; i++) {
+
+
+        let result = callback(acc, newCollection[i], newCollection)
+        console.log(result)
+        results += result
+
+    }
+    console.log("END OF FOR LOOP " + results)
+    return results
+
+}
+
+const callback = (acc, val, collection) => (acc + (val * 3))
+myReduce([1, 2, 3, 4], callback, 10)
+
+function myFind(collection, predicate) {
+    let newCollection = standardizeInput(collection)
+
+
+    for (const element of newCollection) {
+        if (predicate(element)) {
+            return element
+        }
+    }
+}
+
+function myFilter(collection, predicate) {
+    let newCollection = standardizeInput(collection)
+
+    let results = []
+
+    for (const element of newCollection) {
+        if (predicate(element)) {
+            results.push(element)
+        }
+    }
+    return results
+
+}
+
+
+
+function mySize(collection) {
+    let newCollection = standardizeInput(collection)
+
+    let counter = 0;
+
+    for (let i = 0; i < newCollection.length; i++) {
+        counter++
+    }
+    return counter
+
+}
+
+
+
+function myFirst() {
+
+}
+
+function myLast() {
+
+}
+
+
+// Don't use the top function, not applicable here, and we know they will be referring to objects
+function myKeys() {
+
+}
+
+function myValues() {
+
+}
